663a511f2829e063921ff6213e88ee788dc7d406
jborkows <jacek.borkowskiit@gmail.com>
2024-09-19 03:52:34 +0200

    Partial success dashboard exposed but access was impossible, but https works. Each route has to be enabled in local dns!

--- install_traeffic.sh
+++ install_traeffic.sh
@@ -1,4 +1,5 @@
 kubectl apply -f metallb-config.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.3/cert-manager.yaml
 kubectl create secret generic dns-api-token --from-literal=DNS_API_TOKEN=<api-token> -n traefik
 helm repo add traefik https://helm.traefik.io/traefik
 helm repo update
